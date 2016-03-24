e006be645d8f08089da7e8776ac0bdc3ae316d31
Dylan Meysmans <dmeysman@vub.ac.be>
2016-03-24 17:14:45 +0100

    Expand master documentation and specifications

--- src/master.erl
+++ src/master.erl
@@ -52,18 +52,27 @@ master_actor(Subscriptions, Receivers, Channels) ->
     {Sender, log_out, UserName} ->
       % We first notify all channels the user subscribes to and dispose of the receiver.
       log_out(Sender, dict:fetch(UserName, Subscriptions), Channels),
+      % We then notify the sender that we successfully logged the user out.
       Sender ! {self(), logged_out},
+      % Finally we proceed with the user removed from our receivers tree.
       master_actor(Subscriptions, gb_trees:delete(UserName, Receivers), Channels);
 
     {Sender, join_channel, UserName, ChannelName} ->
+      % We first subscribe the user to the channel.
       NewSubscriptions = dict:update(UserName, subscribe(ChannelName), Subscriptions),
+      % We then spawn a new channel process if the channel does not exist yet.
       NewChannels = find_or_create_channel(gb_trees:values(Receivers), ChannelName, Channels),
+      % Now we notify the channel that a user wishes to join it.
       dict:fetch(ChannelName, NewChannels) ! {self(), join_channel, {user, UserName, Sender}},
+      % We do not forget to notify the sender that the user has successfully joined the channel.
       Sender ! {self(), channel_joined},
+      % Finally, we proceed with the new state.
       master_actor(NewSubscriptions, Receivers, NewChannels);
 
     {Sender, get_channel_history, ChannelName} ->
+      % We forward requests for channel histories to the channel processes.
       dict:fetch(ChannelName, Channels) ! {Sender, get_channel_history},
+      % We then proceed with the same state.
       master_actor(Subscriptions, Receivers, Channels)
   end.
 
@@ -74,14 +83,26 @@ log_in(UserPid, {user, SubscriberName, Subscriptions}, Channels) ->
   % We first create a receiver for the user.
   Receiver = receiver:initialize_with(Channels),
   % We then notify all channels the user subscribes to that he wishes to join them.
+  %   We use a list comprehension here instead of lists:foreach/2, because the
+  %   compiler optimizes the construction of the result list away, as per
+  %   http://erlang.org/doc/efficiency_guide/listHandling.html#id67631.
   _ = [dict:fetch(Subscription, Channels) ! {self(), join_channel, {user, SubscriberName, UserPid}} || Subscription <- sets:to_list(Subscriptions)],
   % Finally, we return the process identifier of the newly created receiver.
   Receiver.
 
+-spec log_out(UserPid  :: pid(),
+              User     :: {user, string(), sets:set(string())},
+              Channels :: dict:dict(string(), pid())) -> any().
 log_out(UserPid, {user, SubscriberName, Subscriptions}, Channels) ->
   % We notify all channels the user subscribes to that he wishes to leave them.
+  %   We use a list comprehension here instead of lists:foreach/2, because the
+  %   compiler optimizes the construction of the result list away, as per
+  %   http://erlang.org/doc/efficiency_guide/listHandling.html#id67631.
   _ = [dict:fetch(Subscription, Channels) ! {self(), leave_channel, {user, SubscriberName, UserPid}} || Subscription <- sets:to_list(Subscriptions)].
 
+-spec find_or_create_channel(ReceiverPids :: [pid()],
+                             ChannelName  :: string(),
+                             Channels     :: dict:dict(string(), pid())) -> dict:dict(string(), pid()).
 find_or_create_channel(ReceiverPids, ChannelName, Channels) ->
   case dict:find(ChannelName, Channels) of
     {ok, _} ->
