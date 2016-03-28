e847414a0acb19da949b8bf4e91ee08d0d5549f5
Dylan Meysmans <dmeysman@vub.ac.be>
2016-03-28 17:23:44 +0200

    Notify subscribed channels of login asynchronously
    It is wasteful to make the master process block because channels need to be notified of a user logging in.

--- src/master.erl
+++ src/master.erl
@@ -37,11 +37,20 @@ master_actor(Subscriptions, Channels) ->
       master_actor(NewSubscriptions, Channels);
 
     {Sender, log_in, UserName} ->
-      % We first create a receiver and notify all channels the user subscribes to.
-      Receiver = log_in(Sender, dict:fetch(UserName, Subscriptions), Channels),
-      % We then tell the user to send all future messages to its receiver
-      %   instead of us.
-      Sender ! {Receiver, logged_in},
+      % We first create a receiver for the user and tell him to send all future
+      %   messages to his receiver instead of us.
+      Sender ! {receiver:initialize_with(Channels), logged_in},
+      % We now asynchronously notify all channels the user subscribes to of his presence,
+      %   unless he has no subscriptions, e.g. in the case of a newly registered user.
+      {user, UserName, UserSubscriptions} = dict:fetch(UserName, Subscriptions),
+      NumberOfSubscriptions = sets:size(UserSubscriptions),
+      if
+        % We could use a higher value here and handle very small sets synchronously.
+        NumberOfSubscriptions > 0 ->
+          spawn_link(?MODULE, log_in, [Sender, {user, UserName, UserSubscriptions}, Channels]);
+        true ->
+          ok
+      end,
       % Finally, we proceed.
       master_actor(Subscriptions, Channels);
 
@@ -76,17 +85,14 @@ master_actor(Subscriptions, Channels) ->
 
 -spec log_in(UserPid  :: pid(),
              User     :: {user, string(), sets:set(string())},
-             Channels :: dict:dict(string(), pid())) -> pid().
+             Channels :: dict:dict(string(), pid())) -> ok.
 log_in(UserPid, {user, SubscriberName, Subscriptions}, Channels) ->
-  % We first create a receiver for the user.
-  Receiver = receiver:initialize_with(Channels),
-  % We then notify all channels the user subscribes to that he wishes to join them.
+  % We notify all channels the user subscribes to that he wishes to join them.
   %   We use a list comprehension here instead of lists:foreach/2, because the
   %   compiler optimizes the construction of the result list away, as per
   %   http://erlang.org/doc/efficiency_guide/listHandling.html#id67631.
   _ = [dict:fetch(Subscription, Channels) ! {self(), join_channel, {user, SubscriberName, UserPid}} || Subscription <- sets:to_list(Subscriptions)],
-  % Finally, we return the process identifier of the newly created receiver.
-  Receiver.
+  ok.
 
 -spec log_out(UserPid  :: pid(),
               User     :: {user, string(), sets:set(string())},
